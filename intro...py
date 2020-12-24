79d91a73525e5a3fb8231c90698d999c56fb2502
BAH12 <abubakarrbah90@gmail.com>
2020-12-24 13:48:35 +0100

    first commit

---
+++ intro...py
@@ -0,0 +1,28 @@
+WHAT IS PYTHON ?
+Python is a popular Programming Language.It was created by Guido Van Rossum and released in 1991
+
+IT IS USED FOR 
+Web Development
+Software Development
+Mathematics
+System Scripting
+
+WHAT PYTHON CAN DO
+Python can be used on servers to create web applications
+Python can be used alongside software to create workflows
+Python can connect to database systems.It can also read and modify files
+Python can be used to handle big data and perform complex mathematics
+Python can be used for rapid prototyping ,or for productions-ready software development
+
+WHY PYTHON ?
+Python works on different platforms(Windows, Mac, Linux, Rasberry Pi)
+Python has a simple syntax similar to the English Language
+Python has syntax that allowed developers to write program with fewer lines of code than some other Programming languages
+Python runs on an interpreter system. that means, code can be excuted as soon as it is written.This is means prototyping can be very quick
+Python can be treated in procedural way ,an Object-Oriented-Language or a funtional way 
+
+GOOD TO KNOW 
+The most recent major version of Python is Python 3
+Which we shall be using in this tutorial 
+And also Python can be written on a text editor such as vs code, Atom, Sublime 
+It is also possible to write Python on an Integrated Development Environment (IDE) such as Pycharm,netbeans,eclipse
