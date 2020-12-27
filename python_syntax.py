ab13c3f5b905bc5331fb0fd92beaf5cbfb8049aa
BAH12 <abubakarrbah90@gmail.com>
2020-12-27 21:10:50 +0100

    Python Syntax added

---
+++ python_syntax.py
@@ -0,0 +1,37 @@
+                        PYTHON SYNTAX
+Execute Python Syntax
+Python syntax can be executed by writting directly in the command line
+>>>print('Hello, Python Programmers')
+The result will be (Hello, Python Programmers)
+or by creating a python file on the server, using the .py extension, and running it in the command line
+"C:\\Users\Your Name>myfile.py"
+
+                    PYTHON INDENTATION
+                    '.'
+Indentation refers to the spaces at the beginning of a code line
+Where in other Programming languages the indentation is for readability only, The indentation in python is very important
+Python uses indentation to indicate a block of code 
+if 5 > 2:
+    print('Five is greater than two')
+Python will give you an error if you skip the indentation
+if 5 > 2:
+print('Five is greater than two')
+The number of spaces is up to you as a programmer,but it has to be at least one
+
+You have to use the same number of spaces inthe same block of code
+Otherwise python will give you an error
+if 5 > 2:
+    print('Five is greater than two')
+        print('Five is greater than two')
+    
+
+                    PYTHON VARIABLES
+In Python variables are created when you assign a value to it
+x = 5
+y = 'Hello World'
+
+                         PYTHON COMMENT
+Python has a commenting capability for the purpose of in code documentation..
+Comment start with a #, and python will render the rest of the line as a comment 
+# This is comment
+print('Hello World')
