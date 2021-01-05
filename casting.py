8b56da81eec5103d8393e6a24cf6139952b53844
BAH12 <abubakarrbah90@gmail.com>
2021-01-05 21:40:11 +0100

    Python Casting Added

---
+++ casting.py
@@ -0,0 +1,34 @@
+                                    PYTHON CASTING
+There may be times when you want to specify a type on a variable
+This can be done with casting 
+Python is an object-Oriented-Language ,and as such it uses classes to define data types, including its primitive types.
+
+Casting in python is therefore done using constructor functions
+
+
+                                INT()
+INT()- Constructs an integer number from an integer literal (by rounding down to the previous number) or a string literal (providing the string represent a whole number)
+
+
+                                FLOAT()
+FLOAT()- constructs a float number from an integer literal, a float literal or a string literal ( providing the string represents a float or an integer)
+
+
+                                STR()
+STR()- constructs a string from a wide variety of data types, including strings, integer literals and float literals
+
+
+INTEGER:
+x = int(1)      # x will be 1
+y = int(2.8)    # y will be 2
+z = int('3')    # z will be 3
+
+FLOAT:
+x = float(1)    # x will be  1.0
+y = float(2.8)  # y will be 2.8
+z = float('3')  # z will be 3.0
+
+STRING:
+x = str('s1')  # x will be 's1'
+y = str(2)     # y will be '2'
+z = str(3.0)   # z will be '3.0'
