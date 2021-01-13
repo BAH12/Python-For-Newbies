f45c384f83d91addbb7c2a43c44f04701bc48b7d
BAH12 <abubakarrbah90@gmail.com>
2021-01-14 00:08:46 +0100

    Python Operators added

---
+++ operators.py
@@ -0,0 +1,148 @@
+                                PYTHON OPERATORS
+Operators are used to perform operations on variables and values
+In the example below, we use the + operator to add together two values
+print(10 + 5)
+
+Python divides the operators in the following groups
+Arithmetic operators
+
+Assignment operators
+
+Comparison operations
+
+Logical operators
+
+Identity operations
+
+Membership operators
+
+Bitwise operators
+
+                        PYTHON ARITHMETIC OPERATORS
+Arithmetic operators are used with numeric values to perform common mathematical operations
+
+OPERATOR            NAME                        EXAMPLE
++                   Addition                   x + y
+
+-                   Subtraction                 x - y
+
+*                   Multiplication              x * y
+
+/                   Division                    x / y
+
+%                   Modulus                     x % y 
+
+**                  Exponentiation               x ** y
+
+//                  Floor Division              x // y
+
+
+
+                        PYTHON ASSIGNMENT OPERATORS
+Assignment operators are used to assign values to variables
+
+OPERATOR                    EXAMPLE                        SAME AS
+=                           x = 5                          x = 5
+
++=                          x += 3                         x = x+ 3
+
+-=                          x -= 3                         x = x- 3
+
+*=                          x *= 3                         x = x* 3
+
+/=                          x /= 3                         x = x/ 3
+
+
+%=                          x %= 3                         x = x% 3
+
+
+//=                         x //= 3                        x = x// 3
+
+**=                         x **= 3                        x = x** 3
+
+&=                          x &= 3                         x = x& 3
+
+|=                          x |= 3                         x = x| 3
+
+^=                          x ^= 3                         x = x^ 3
+
+>>=                         x >>= 3                        x = x>> 3
+
+<<=                         x <<= 3                        x = x<< 3
+
+
+
+                        PYTHON COMPARISON OPERATORS
+Comparison operators are uses to compare two values
+
+OPERATOR            NAME                                EXAMPLE
+==                  Equal                               x == y
+
+!=                  Not Equal                           x != y
+
+
+>                   Greater Than                        x > y
+
+<                   Less Than                           x < y
+
+>=                  Greater Than or Equal to            x >= y
+
+<=                  Less Than or Equal to               x <= y
+
+
+                        PYTHON LOGICAL OPERATORS
+Logical operators are used to combine conditional statement
+
+OPERATOR        DESCRIPTION                        EXAMPLE
+and             Returns True if both statement     x < y and x < 10
+                are true
+
+or              Returns True if one of
+                thestatement is true                x < y or y > 15
+
+not             Reverse the result, returns
+                False if the result is true
+
+
+                        PYTHON IDENTITY OPERATORS
+Identity operators are used to compare the object, not if they are equal, but if they are actually the same object, with the same memory location
+
+OPERATOR        DESCRIPTION                        EXAMPLE
+is              Returns True if both variables
+                are the same object                 x is y
+
+is not          Returns True if both variables
+                are not the same object
+
+
+                        PYTHON MEMBERSHIP OPERATORS
+Membership operators are used to test if a sequence is presented in an object
+
+OPERATOR                            DESCRIPTION                      
+in                                  Returns True if a sequence with 
+                                    the specified value is present in 
+                                    the object
+
+not in                              Returns True if a sequence with
+                                    the specified value is not present
+                                    in the object
+
+
+                    PYTHON BITWISE OPERATORS
+Bitwise operators are used to compare (binary) numbers
+
+OPERATOR        NAME                DESCRIPTION
+&               AND                 Sets each bit 1 if  both bits are 1
+
+|               OR                  Sets bit to 1 if one of two bits is 1
+
+^               XOR                 Sets each bit to 1 if only one of two bits is 1
+
+~               NOT                 Inverts all the bits
+
+<<              Zero fill left      Shift left by pushing zeros in from the right and
+                shift               let the leftmost bits fall off
+
+>>              Signed right        Shift right by pushing copies of the leftmost bit
+                shift               in from the left, and let the rightmost bit
+                                    fall off
